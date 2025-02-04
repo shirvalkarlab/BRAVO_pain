879b3413846b0a39bf207eb9a6515616efb4df50
JCagle95 <jackson.cagle@gmail.com>
2025-02-04 11:30:13 -0800

    rm also require sudo

--- BRAVO/update.sh
+++ BRAVO/update.sh
@@ -11,7 +11,7 @@ python3 manage.py migrate
 npm install --prefix $SCRIPT_DIR/../Client
 npm run build --prefix $SCRIPT_DIR/../Client
 python3 $SCRIPT_DIR/manage.py create_template
-rm -r /usr/share/nginx/html/*
+sudo rm -r /usr/share/nginx/html/*
 sudo cp -r $SCRIPT_DIR/../Client/build/* /usr/share/nginx/html/
 
 sudo systemctl restart bravo_server 
